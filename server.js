1de862b83bb64b2f976ef0f2f935228eec0757eb
Tushar Vispute <tusharvispute07@gmail.com>
2023-11-07 20:42:17 +0530

    another change to cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 4000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 const corsOptions = {
-    origin: "*",
+    origin:  ['https://taskplanner-app.onrender.com', 'http://localhost:3000'],
     credentials: true,
     optionsSuccessStatus: 200 
 } 
